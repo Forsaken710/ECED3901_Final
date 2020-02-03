a3087ba39a1f9841a09d934f944750a9add29c24
Vincent Sieben <sieben@dal.ca>
2020-02-03 19:54:47 +0000

    Initialized test package for ECED3901 Labortary 0

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/my_catkin_ws/src
+# Build directory: /home/ubuntu/my_catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("eced3901_test")
---
+++ build/eced3901_test/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/my_catkin_ws/src/eced3901_test
+# Build directory: /home/ubuntu/my_catkin_ws/build/eced3901_test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
